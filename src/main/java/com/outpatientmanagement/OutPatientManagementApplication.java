19b1979e749f41c025fd609706d36416aa836546
Jayashrihemalatha56 <hshri8516@gmail.com>
2026-01-14 17:41:17 +0530

    Created Spring Starter Project and Added Dependencies

---
+++ src/main/java/com/outpatientmanagement/OutPatientManagementApplication.java
@@ -0,0 +1,13 @@
+package com.outpatientmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutPatientManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutPatientManagementApplication.class, args);
+	}
+
+}
